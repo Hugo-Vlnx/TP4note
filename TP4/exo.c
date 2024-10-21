3f49c8de9b7d848107cd982cc420b6d9aa4daa6b
Hugo.V <hugo.vialaneix@gmail.com>
2024-10-21 10:03:30 +0200

    exo 1, version 1.0

--- TP4/exo.c
+++ TP4/exo.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
+
+
+int grilleInitiale(int grille[9][9], int n ) //n représente la taille de la grille
+{
+	
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			grille[i][j]=0;
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d", grille[i][j]);
+
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
+
+int main()
+{
+	int n = 5;
+	int grille[9][9];
+	grilleInitiale(grille, n);
+	return 0;
+
+
+}
