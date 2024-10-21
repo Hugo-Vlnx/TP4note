9997ffa842106f7a9eb53b4b3938104cbd44267b
Hugo.V <hugo.vialaneix@gmail.com>
2024-10-21 10:48:45 +0200

    exo 2  2.0

--- TP4/exo.c
+++ TP4/exo.c
@@ -19,7 +19,7 @@ int grilleInitiale(int grille[9][9], int n ) //n représente la taille de la gril
 		for (int j = 0; j < n; j++)
 		{
 			printf("%d", grille[i][j]);
-
+			printf(" ");
 		}
 		printf("\n");
 	}
@@ -27,11 +27,51 @@ int grilleInitiale(int grille[9][9], int n ) //n représente la taille de la gril
 	return 0;
 }
 
+int remplirGrille(int grille[9][9], int n)
+{
+	int ligne = 0;
+	int colonne = 0;
+	int valeur = 0;
+	int nombreDeBoucle = 0;
+	printf("combien de case voulez vopus remplir");
+	scanf_s("%d", &nombreDeBoucle);
+
+	for(int i = 0; i < nombreDeBoucle; i++)
+	{
+		printf("entrer la ligne:");
+		scanf_s("%d", &ligne);
+		printf("entrer la colonne:");
+		scanf_s("%d", &colonne);
+		printf("entrer la valeur:");
+		scanf_s("%d", &valeur);
+		if (ligne < n && colonne < n)
+		{
+			grille[ligne][colonne] = valeur;
+		}
+		else
+		{
+			printf("cette case n'existe pas \n");
+			i--;
+
+		}
+		
+		
+	}
+
+	return 0;
+
+
+}
+
 int main()
 {
-	int n = 5;
+	int n = 2;
 	int grille[9][9];
 	grilleInitiale(grille, n);
+	remplirGrille(grille, n);
+
+
+
 	return 0;
 
 
